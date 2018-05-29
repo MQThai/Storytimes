b617f267b7c9b0aabf5fd68c2b85b2343c8006da
Minhquan Thai <minhquanthai@gmail.com>
2018-05-29 09:49:55 -0400

    Added planning

--- backend/storytimes/models.py
+++ backend/storytimes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Prompt(models.Model):
+    setting = models.CharField(max_length=500)
+    author = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.setting
+
+class Post(models.Model):
+    content = models.TextField()
+    author = models.CharField(max_length=25)
+
